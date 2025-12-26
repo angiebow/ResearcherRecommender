f0ca321ba28679f7bf68c063ef3699e13b55b0a5
Pelangi Masita <pelangimasita18@gmail.com>
2025-12-26 15:38:35 +0700

    Style: accordion relocation

--- frontend/portal/app/page.tsx
+++ frontend/portal/app/page.tsx
@@ -65,8 +65,101 @@ export default function RecommenderPage() {
     <div>
        <h1 className="text-3xl font-bold mb-2">Researcher Recommendation</h1>
       <p className="text-slate-400 mb-8">Enter a research topic to find the most relevant experts from the institution.</p>
+      
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
+        <div>
+          <label className="block text-sm font-medium text-slate-400 mb-2">Select Model</label>
+          <Select value={model} onValueChange={setModel}>
+            <SelectTrigger
+              className="
+                h-12 text-md
+                bg-white border-gray-300 text-gray-900
+                dark:bg-slate-800 dark:border-slate-700 dark:text-gray-100
+              "
+            >
+            <SelectValue /></SelectTrigger>
+            <SelectContent>
+              <SelectItem value="BERT">BERT</SelectItem>
+              <SelectItem value="DistilBERT">DistilBERT</SelectItem>
+              <SelectItem value="Albert">Albert</SelectItem>
+              <SelectItem value="XLNet">XLNet</SelectItem>
+              <SelectItem value="MPNet">MPNet</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-slate-400 mb-2">Select Metric</label>
+          <Select value={metric} onValueChange={setMetric}>
+          <SelectTrigger
+            className="
+              h-12 text-md
+              bg-white border-gray-300 text-gray-900
+              dark:bg-slate-800 dark:border-slate-700 dark:text-gray-100
+            "
+          >
+          <SelectValue /></SelectTrigger>
+            <SelectContent>
+              <SelectItem value="Cosine Similarity">Cosine</SelectItem>
+              <SelectItem value="Hamming">Hamming</SelectItem>
+              <SelectItem value="Jaccard">Jaccard</SelectItem>
+              <SelectItem value="Minkowski">Minkowski</SelectItem>
+              <SelectItem value="Kullback-Leibler">Kullback-Leibler</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
 
-      <Accordion type="multiple" className="mb-10">
+      <div className="flex w-full items-start gap-2 mb-10">
+        <Textarea
+          placeholder="Input Topic in English"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="
+            flex-1 min-w-0 w-full min-h-[120px] resize-none text-md
+            bg-white border-gray-300 text-gray-900
+            dark:bg-slate-800 dark:border-slate-700 dark:text-gray-100
+          "
+        />
+
+        <Button
+          size="lg"
+          onClick={handleSearch}
+          disabled={isLoading}
+          className="h-12 mt-1 shrink-0"
+        >
+          <Search className="mr-2 h-5 w-5" /> Search
+        </Button>
+      </div>
+      
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+        {isLoading && (
+          Array.from({ length: 8 }).map((_, i) => <ResearcherCardSkeleton key={i} />)
+        )}
+        {!isLoading && results.map((researcher, index) => (
+          <ResearcherCard
+            key={index}
+            rank={index + 1}
+            name={researcher.name}
+            score={researcher.score}
+            //faculty={researcher.faculty}
+            //department={researcher.department}
+            //research_center={researcher.research_center}
+            metric={metric} 
+            //focus_topics={researcher.focus_topics}
+          />
+        ))}
+      </div>
+      
+      {!isLoading && hasSearched && results.length === 0 && (
+          <div className="col-span-3 text-center py-10">
+            {hasSearched ? 
+              <p className="text-slate-500">No results found. Please try a different query.</p> :
+              <p className="text-slate-500">Please enter a topic to begin your search.</p>
+            }
+          </div>
+      )}
+
+            <Accordion type="multiple" className="mb-10">
         {/* NDCG@5 */}
         <AccordionItem value="ndcg">
           <AccordionTrigger className="text-lg font-semibold">
@@ -163,99 +256,6 @@ export default function RecommenderPage() {
           </AccordionContent>
         </AccordionItem>
       </Accordion>
-      
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
-        <div>
-          <label className="block text-sm font-medium text-slate-400 mb-2">Select Model</label>
-          <Select value={model} onValueChange={setModel}>
-            <SelectTrigger
-              className="
-                h-12 text-md
-                bg-white border-gray-300 text-gray-900
-                dark:bg-slate-800 dark:border-slate-700 dark:text-gray-100
-              "
-            >
-            <SelectValue /></SelectTrigger>
-            <SelectContent>
-              <SelectItem value="BERT">BERT</SelectItem>
-              <SelectItem value="DistilBERT">DistilBERT</SelectItem>
-              <SelectItem value="Albert">Albert</SelectItem>
-              <SelectItem value="XLNet">XLNet</SelectItem>
-              <SelectItem value="MPNet">MPNet</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-slate-400 mb-2">Select Metric</label>
-          <Select value={metric} onValueChange={setMetric}>
-          <SelectTrigger
-            className="
-              h-12 text-md
-              bg-white border-gray-300 text-gray-900
-              dark:bg-slate-800 dark:border-slate-700 dark:text-gray-100
-            "
-          >
-          <SelectValue /></SelectTrigger>
-            <SelectContent>
-              <SelectItem value="Cosine Similarity">Cosine</SelectItem>
-              <SelectItem value="Hamming">Hamming</SelectItem>
-              <SelectItem value="Jaccard">Jaccard</SelectItem>
-              <SelectItem value="Minkowski">Minkowski</SelectItem>
-              <SelectItem value="Kullback-Leibler">Kullback-Leibler</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
-
-      <div className="flex w-full items-start gap-2 mb-10">
-        <Textarea
-          placeholder="Input Topic in English"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          className="
-            flex-1 min-w-0 w-full min-h-[120px] resize-none text-md
-            bg-white border-gray-300 text-gray-900
-            dark:bg-slate-800 dark:border-slate-700 dark:text-gray-100
-          "
-        />
-
-        <Button
-          size="lg"
-          onClick={handleSearch}
-          disabled={isLoading}
-          className="h-12 mt-1 shrink-0"
-        >
-          <Search className="mr-2 h-5 w-5" /> Search
-        </Button>
-      </div>
-      
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
-        {isLoading && (
-          Array.from({ length: 8 }).map((_, i) => <ResearcherCardSkeleton key={i} />)
-        )}
-        {!isLoading && results.map((researcher, index) => (
-          <ResearcherCard
-            key={index}
-            rank={index + 1}
-            name={researcher.name}
-            score={researcher.score}
-            //faculty={researcher.faculty}
-            //department={researcher.department}
-            //research_center={researcher.research_center}
-            metric={metric} 
-            //focus_topics={researcher.focus_topics}
-          />
-        ))}
-      </div>
-      
-      {!isLoading && hasSearched && results.length === 0 && (
-          <div className="col-span-3 text-center py-10">
-            {hasSearched ? 
-              <p className="text-slate-500">No results found. Please try a different query.</p> :
-              <p className="text-slate-500">Please enter a topic to begin your search.</p>
-            }
-          </div>
-      )}
     </div>
   );
 }
